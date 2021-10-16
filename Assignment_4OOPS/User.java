8a888dc770eec5dfe6d8be40521342f3fa195975
TusharDeotale <tushardeotale77@gmail.com>
2021-10-16 21:42:55 +0530

    code changed

--- Assignment_4OOPS/User.java
+++ Assignment_4OOPS/User.java
@@ -1,10 +1,31 @@
 package oops;
 
+import java.util.Scanner;
+
 public class User {
+	int a = 0;
+
+	User() {
+		System.out.println(a);
+	}
+
+	User(int a) {
+		this.a = a;
+		System.out.println(this.a);
+	}
 
 	public static void main(String[] args) {
-		
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n1 = sc.nextInt();
+		System.out.println("Type again!!");
+		boolean n = sc.hasNextInt();
+		if (n) {
+			 new User(n1);
+		} else {
+			 new User();
+		}
+		sc.close();
 	}
 
 }
