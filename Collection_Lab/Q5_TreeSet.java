b3f02a787cd8578c0415bf33d4fd3c4b921c14f1
TusharDeotale <tushardeotale77@gmail.com>
2021-11-11 01:34:54 +0530

    completed!

---
+++ Collection_Lab/Q5_TreeSet.java
@@ -0,0 +1,19 @@
+package collectionAssignment;
+
+import java.util.TreeSet;
+
+public class Q5_TreeSet {
+
+	public static void main(String[] args) {
+		TreeSet<String> t = new TreeSet<String>();
+		t.add("Red");
+		t.add("Orange");
+		t.add("Yellow");
+		t.add("Green");
+		t.add("Blue");
+		t.add("Indigo");
+		t.add("Voilet");
+		System.out.println(t);
+	}
+
+}
